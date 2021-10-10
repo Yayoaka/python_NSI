c655277967bbfc44543f5903ce454ff7dea776bd
Yayoaka <76622183+Yayoaka@users.noreply.github.com>
2021-10-10 11:47:21 +0200

    Create script1.py

---
+++ turtle/script1.py
@@ -0,0 +1,15 @@
+def operation_1(a,b,c):
+    return (a+b*c)
+def operation_2(a,b,c):
+    return (a*2+b)
+def operation_3(a,b,c):
+    return (c*3-b)
+i=5
+j=1
+k=2
+resultat_1=operation_1(i,j,k)+opration_3(k,i,j)
+if i!=resultat_1:
+    resultat_2=resultat_1+operation_2(k,j,i)
+else:
+    resultat_2=resultat_1+operation_2(k,i,j)
+print(resultat_2)
